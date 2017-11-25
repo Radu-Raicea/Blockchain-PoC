894de75772e99e5793a3f41a9989cb82eebc092e
r <radu@raicea.com>
2017-11-25 16:42:49 -0500

    add consensus

--- api.py
+++ api.py
@@ -1,4 +1,6 @@
 
+from uuid import uuid4
+
 from flask import Flask, request, jsonify
 
 import pow
@@ -7,6 +9,7 @@ from blockchain import Blockchain
 app = Flask(__name__)
 
 blockchain = Blockchain()
+miner_address = str(uuid4())
 
 
 @app.route('/transactions', methods=['POST'])
@@ -26,16 +29,11 @@ def create_transaction():
 
 @app.route('/blocks', methods=['POST'])
 def create_block():
-    miner = request.get_json()
-
-    if 'miner_address' not in miner:
-        return 'Invalid keys in request.', 400
-
     previous_block = blockchain.previous_block
     previous_proof = previous_block['proof']
     new_proof = pow.generate_new_proof(previous_proof)
 
-    blockchain.new_transaction('0', miner['miner_address'], 1)
+    blockchain.new_transaction('0', miner_address, 1)
 
     previous_hash = blockchain.hash_block(previous_block)
     block = blockchain.new_block(new_proof, previous_hash)
@@ -53,11 +51,41 @@ def create_block():
 
 @app.route('/blocks', methods=['GET'])
 def read_blocks():
-    blocks = {
-        'blockchain': blockchain.chain,
-        'length': len(blockchain.chain)
-    }
+    resolved = blockchain.resolve_chain_conflicts()
+
+    if resolved:
+        blocks = {
+            'message': 'Your chain is valid.',
+            'blockchain': blockchain.chain,
+            'length': len(blockchain.chain)
+        }
+    else:
+        blocks = {
+            'message': 'Your chain was not valid. It was updated.',
+            'blockchain': blockchain.chain,
+            'length': len(blockchain.chain)
+        }
+
     return jsonify(blocks), 200
 
+
+@app.route('/nodes', methods=['POST'])
+def create_node():
+    response = request.get_json()
+
+    if 'nodes' not in response:
+        return 'Invalid keys in request.', 400
+
+    nodes = response['nodes']
+
+    for node in nodes:
+        blockchain.new_node(node)
+
+    response = {
+        'message': 'Node added succesfully.',
+        'number_of_nodes': len(blockchain.nodes)
+    }
+    return jsonify(response), 201
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5003)
--- blockchain.py
+++ blockchain.py
@@ -2,6 +2,11 @@
 import datetime
 import json
 import hashlib
+import urllib.parse as url
+
+import requests
+
+import pow
 
 
 class Blockchain(object):
@@ -9,6 +14,7 @@ class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.pending_transactions = []
+        self.nodes = set()
         self.new_block(100, 1)
 
     def new_block(self, proof, previous_hash=None):
@@ -55,6 +61,66 @@ class Blockchain(object):
         })
 
         return self.previous_block['index'] + 1
+
+    def new_node(self, address):
+        """
+        Adds a new node to our blockchain nodes list.
+
+        :param str address: URL address of the node
+        :return: None
+        """
+
+        parsed_address = url.urlparse(address)
+        self.nodes.add(parsed_address.netloc)
+
+    @classmethod
+    def validate_chain(cls, chain):
+        """
+        Validates the blockchain of a node.
+
+        :param list chain: Blockchain of a node
+        :return bool: True if the chain is valid, False if not
+        """
+
+        previous_block = chain[0]
+
+        for block in chain[1:]:
+            if block['previous_hash'] != cls.hash_block(previous_block):
+                return False
+
+            if not pow.validate_proof(previous_block['proof'], block['proof']):
+                return False
+
+            previous_block = block
+
+        return True
+
+    def resolve_chain_conflicts(self):
+        """
+        Resolves blockchain conflicts by replacing our chain with the longest valid one.
+
+        :return bool: True if our chain was replaced, False if not
+        """
+
+        new_chain = None
+        longest_length = len(self.chain)
+
+        for node in self.nodes:
+            response = requests.get(f'http://{node}/blocks')
+
+            if response.status_code == 200:
+                chain = response.json()['blockchain']
+                length = response.json()['length']
+
+                if length > longest_length and self.validate_chain(chain):
+                    new_chain = chain
+                    longest_length = length
+
+        if new_chain is not None:
+            self.chain = new_chain
+            return True
+
+        return False
 
     @staticmethod
     def hash_block(block):
